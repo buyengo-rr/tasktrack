c60cff2bc3e344ab0849029c4bd2e80e9964b216
diagne <diagnebuyengo@gmail.com>
2025-06-04 20:13:14 +0300

    Display task management menu options

--- lib/cli.py
+++ lib/cli.py
@@ -1,1 +1,17 @@
 from main import list_tasks, add_task, view_task, complete_task, delete_task, upcoming_tasks
+
+def main():
+    while True:
+        print("\n⏳ TASKFLOW CLI")
+        print("1. 📋 List Tasks")
+        print("2. ➕ Add Task")
+        print("3. 🔍 View Task")
+        print("4. ✅ Mark as Complete")
+        print("5. 🗑️ Delete Task")
+        print("6. 📆 View Upcoming Tasks")
+        print("0. 🚪 Exit")
+        
+        choice = input("Choose: ")
+
+
+
